9698f877ad363fa6b808d2dbd525dde2e2248d2c
Gentility01 <mastergentility5@gmail.com>
2025-11-27 02:18:10 +0100

    A new fix

--- core/applications/users/views.py
+++ core/applications/users/views.py
@@ -6,8 +6,12 @@ from django.utils.translation import gettext_lazy as _
 from django.views.generic import DetailView
 from django.views.generic import RedirectView
 from django.views.generic import UpdateView
+from django.views.generic import CreateView
 
+from core.applications.users.forms import SuperCustomUserCreationForm
 from core.applications.users.models import User
+from django.urls import reverse_lazy
+
 
 
 class UserDetailView(LoginRequiredMixin, DetailView):
@@ -44,3 +48,12 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
 
 
 user_redirect_view = UserRedirectView.as_view()
+
+class SuperUserSignupView(CreateView):
+    model = User
+    form_class = SuperCustomUserCreationForm
+    template_name = "users/superuser_signup.html"
+    success_url = reverse_lazy('admin:login')
+
+
+superuser_signup = SuperUserSignupView.as_view()
